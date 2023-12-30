89b896e223f10009e66d3a7159a66eceaedd247f
Vitaliy <118558563+Ghosvx@users.noreply.github.com>
2023-12-30 22:40:17 +0200

    xdd

--- script/disableButton.js
+++ script/disableButton.js
@@ -7,3 +7,42 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    function checkOrientation() {
+        // ... (ваш существующий код)
+    }
+
+    function enableSwipe() {
+        const sections = document.querySelectorAll('section');
+        let currentSectionIndex = 0;
+
+        const hammer = new Hammer(document.body);
+
+        hammer.on('swipeleft', function () {
+            if (currentSectionIndex < sections.length - 1) {
+                currentSectionIndex++;
+                scrollToCurrentSection();
+            }
+        });
+
+        hammer.on('swiperight', function () {
+            if (currentSectionIndex > 0) {
+                currentSectionIndex--;
+                scrollToCurrentSection();
+            }
+        });
+    }
+
+    function scrollToCurrentSection() {
+        const sections = document.querySelectorAll('section');
+        const currentSection = sections[currentSectionIndex];
+        currentSection.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        checkOrientation();
+        enableSwipe();
+        window.addEventListener('orientationchange', checkOrientation);
+    }
+});
